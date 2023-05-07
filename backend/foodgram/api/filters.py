7e05182afea90f316df3a2bee35a11178ed0682a
Nikolay Nikitenko <nikitenkonik@mail.ru>
2023-05-07 16:31:10 +0300

    Review

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -16,7 +16,6 @@ class RecipeFilter(FilterSet):
         queryset=Tag.objects.all(),
         field_name="tags__slug",
         to_field_name="slug",
-        exclude=True
     )
     is_favorited = filters.BooleanFilter(method="is_favorited_filter")
     is_in_shopping_cart = filters.BooleanFilter(method="is_in_shopping_cart_filter")
